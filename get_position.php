fb963803c0b2b387d44009c35536f98de38909e0
MittProd999 <voortimothy@gmail.com>
2024-05-24 11:12:39 +0200

    deleted useless files

--- get_position.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Simuleer het ophalen van de positie vanuit een database of een ander bron
-$lat = 51.54827117919922; // Voorbeeld breedtegraad
-$lng = 4.5983967781066895; // Voorbeeld lengtegraad
-
-// Stuur de positie terug als JSON
-echo json_encode(array('lat' => $lat, 'lng' => $lng));
-?>
