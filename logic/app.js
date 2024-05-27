bcdbf0e7655182addf6459133264cf6bf8edb317
nix18net <hoiikbenmaurits04@gmail.com>
2024-05-27 12:53:40 +0200

    generate flowers

--- logic/app.js
+++ logic/app.js
@@ -4,12 +4,6 @@ let map;
 let userMarker;
 const flowerMarkers = [];
 const flowersCollected = new Set();
-const flowerLocations = [
-  { lat: 51.54827117919922, lng: 4.5983967781066895 },
-  { lat: 51.5569, lng: 4.5983967781066895 },
-  { lat: 51.585935913488875, lng: 4.79281179602562 },
-  { lat: 51.540415, lng: 4.607396}
-];
 
 function initMap(userLocation) {
   map = new mapboxgl.Map({
@@ -19,18 +13,10 @@ function initMap(userLocation) {
     zoom: 16,
   });
 
-  flowerLocations.forEach(location => {
-    const marker = new mapboxgl.Marker({
-      element: createMarkerElement('../assets/flower-icon.png', 30, 30)
-    })
-    .setLngLat([location.lng, location.lat])
-    .addTo(map);
-
-    flowerMarkers.push(marker);
-  });
+  generateFlowerLocations(userLocation, 10, 1000); // 10 flowers within 1 km radius
 
   userMarker = new mapboxgl.Marker({
-    element: createMarkerElement('../assets/current-location-icon.png', 30, 30)
+    element: createMarkerElement('../assets/current_location_icon.png', 30, 30)
   })
   .setLngLat([userLocation.lng, userLocation.lat])
   .addTo(map);
@@ -120,6 +106,40 @@ function createMarkerElement(url, width, height) {
   element.style.width = `${width}px`;
   element.style.height = `${height}px`;
   return element;
+}
+
+function generateFlowerLocations(center, count, radius) {
+  const isochroneServiceUrl = `https://api.mapbox.com/isochrone/v1/mapbox/walking/${center.lng},${center.lat}?contours_meters=${radius}&polygons=true&access_token=${mapboxgl.accessToken}`;
+
+  fetch(isochroneServiceUrl)
+    .then(response => response.json())
+    .then(data => {
+      const isochrone = data.features[0].geometry.coordinates[0];
+      for (let i = 0; i < count; i++) {
+        const randomPoint = getRandomPointInPolygon(isochrone);
+        const location = { lng: randomPoint[0], lat: randomPoint[1] };
+
+        const marker = new mapboxgl.Marker({
+          element: createMarkerElement('../assets/flower_1.png', 30, 30)
+        })
+        .setLngLat([location.lng, location.lat])
+        .addTo(map);
+
+        flowerMarkers.push(marker);
+      }
+    })
+    .catch(error => console.error('Error fetching isochrone:', error));
+}
+
+function getRandomPointInPolygon(polygon) {
+  const bbox = turf.bbox(turf.polygon([polygon]));
+  let point;
+  while (true) {
+    point = turf.randomPoint(1, { bbox }).features[0].geometry.coordinates;
+    if (turf.booleanPointInPolygon(point, turf.polygon([polygon]))) {
+      return point;
+    }
+  }
 }
 
 window.onload = function() {
